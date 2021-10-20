f9e77d3be0e88b4b8e9d905c8839afc019948e45
unknown <anhkhoa180902@Gmail.com>
2021-10-20 18:38:17 +0700

    uuppp

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,41 @@
 
 wow = new WOW(
     {
-        boxClass:     'wow',      // default
+        boxClass: 'wow',      // default
         animateClass: 'animate__animated', // default
-        offset:       0,          // default
-        mobile:       true,       // default
-        live:         true        // default
+        offset: 0,          // default
+        mobile: true,       // default
+        live: true        // default
     }
-    )
-    wow.init();
+)
+wow.init();
 var User = document.querySelector('.nav-right');
 var modalLogin = document.querySelector('.modal-form');
-    function show() {
-        modalLogin.classList.toggle('open');
-    }
-    User.addEventListener('click',show);
-    modalLogin.addEventListener('click',show);
-    document.querySelector('.modal-login').addEventListener('click', function(e) {
-        e.stopPropagation();
-    })
+var btnMenu = document.querySelector('.btn-menu-bar');
+var menuMobileShow = document.querySelector('.mobile-modal');
+var closeMenu = document.querySelector('.close-mobile').addEventListener('click', btnOpen);
+var modalClickMobile = document.querySelector('.click-account');
+var closeLoginMobile = document.querySelector('.modal-close');
+function show() {
+    modalLogin.classList.toggle('open');
+}
+User.addEventListener('click', show);
+modalLogin.addEventListener('click', show);
+document.querySelector('.modal-login').addEventListener('click', function (e) {
+    e.stopPropagation();
+})
+function btnOpen() {
+    menuMobileShow.classList.toggle('open-menu-mobile');
+}
+btnMenu.addEventListener('click', btnOpen);
+function showMobile() {
+    modalLogin.classList.toggle('open');
+    menuMobileShow.classList.toggle('open-menu-mobile');
+
+}
+function HideMobile() {
+    modalLogin.classList.add('close');
+    menuMobileShow.classList.add('close');
+}
+modalClickMobile.addEventListener('click', showMobile);
+closeLoginMobile.addEventListener('click', showMobile);
